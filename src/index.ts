4e0b3df0beabdd211b9f488156d8e3e1719d4970
すえ <souhait.0614@gmail.com>
2023-06-06 15:35:52 +0900

    0.2.1に合わせて修正

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { generate } from "cjp"
-import { Plugin, type PluginConvertFunction } from "submarin-converter"
+import { Plugin, type ConvertFunction } from "submarin-converter"
 
-const mainConvertFunction: PluginConvertFunction<void> = ({ input }) => generate(input)
+const mainConvertFunction: ConvertFunction<void> = ({ input }) => generate(input)
 
 const cjpPlugin = new Plugin({
   convertFunction: [mainConvertFunction],
