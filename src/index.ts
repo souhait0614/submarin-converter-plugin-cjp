ffef50ed1eb3b920be20703fbf9620fa8a41f65e
すえ <souhait.0614@gmail.com>
2023-06-06 17:15:21 +0900

    metaData追加

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,11 @@ const mainConvertFunction: ConvertFunction<void> = ({ input }) => generate(input
 
 const cjpPlugin = new Plugin({
   convertFunction: [mainConvertFunction],
+  metaData: {
+    name: "怪しい日本語",
+    description: "日本語を正レい日本语に変換します",
+    author: "すえ"
+  }
 })
 
 export { cjpPlugin }
